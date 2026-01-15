279f61dab766c364eda9e037159508fa953c5dae
tutosrive <tutosrivegamer@gmail.com>
2026-01-15 17:25:46 -0500

    First upgrade functional: Missing responsive!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,45 @@
-class App{
-    static init(){
-        App.codeToRun = document.querySelector('#code-run');
-        App.selectOutputMode = document.querySelector('#select-mode');
-        App.codapiSnippet = document.querySelector('#codapi-snippet');
+class App {
+  static init() {
+    App.codeToRun = document.querySelector('#code-run');
+    App.yearFooter = document.querySelector('#year-footer');
+    App.codapiSnippet = document.querySelector('#codapi-snippet-ctn');
 
-        App.insertCodeExample();
-        App.setListeners();
-    }
+    App.insertYearFooter();
+    App.insertCodapiSnippet('table');
+    App.insertCodeExample();
+    App.setListeners();
+  }
 
-    static setListeners(){
-        App.selectOutputMode.addEventListener('change',  e => App.changeOutputMode(e));
-    }
+  static setListeners() {
+    App.selectOutputMode = document.querySelector('#select-mode');
+    App.selectOutputMode.addEventListener('change', (e) => App.changeOutputMode(e));
+  }
 
-    static insertCodeExample(){
-        const code = `const req = await fetch('https://jsonplaceholder.typicode.com/todos/');
+  static insertYearFooter() {
+    App.yearFooter.textContent = new Date().getFullYear();
+  }
+
+  static insertCodapiSnippet(mode = 'plain') {
+    App.codapiSnippet.innerHTML = App.codapiSnippetHTML(mode);
+  }
+
+  static insertCodeExample() {
+    const code = `const req = await fetch('https://jsonplaceholder.typicode.com/todos/');
 const data = await req.json();
 console.log(data);`;
 
-        App.codeToRun.textContent = code;
-    }
+    App.codeToRun.textContent = code;
+  }
 
-    static changeOutputMode(e){
-        e.preventDefault();
-        const mode = e.target.value;
-        App.codapiSnippet.setAttribute('output-mode', mode);
-    }
+  static codapiSnippetHTML(outputMode) {
+    return `<codapi-snippet engine="browser" sandbox="javascript" editor="basic" selector="#js-to-run code" output-mode="${outputMode}"></codapi-snippet>`;
+  }
+
+  static changeOutputMode(e) {
+    e.preventDefault();
+    const mode = e.target.value;
+    App.insertCodapiSnippet(mode);
+  }
 }
 
-App.init()
+App.init();
