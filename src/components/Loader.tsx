ddb3070c35f8a5833fa1da9858f3a2b97849f28b
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2026-01-19 19:30:59 -0500

    Revert "feat: add CodapiCustom component and utility functions
    - Implemented CodapiCustom component for rendering codapi snippets with a toolbar for execution and selector options.
    - Created utility functions for script loading and string manipulation.
    - Defined TypeScript types for codapi snippet attributes to enhance type safety.
    - Added functions to generate options for modes and themes from provided lists."

--- src/components/Loader.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoaderInfinity() {
-  return (
-    <div className="fixed w-[99vw] h-[70vh] flex items-center justify-center">
-      <span className="loading loading-infinity loading-xl"></span>
-    </div>
-  );
-}
--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
-// https://vite.dev
-export default defineConfig({ base: '/eval/', server: { watch: { usePolling: true } }, plugins: [react(), tailwindcss()] });
